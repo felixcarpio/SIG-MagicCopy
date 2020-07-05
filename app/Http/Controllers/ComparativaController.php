dfea197de6483f4ff3acf4e6d636686b92a3a6ec
felixcarpio <felixcarpio9312@gmail.com>
2020-07-04 23:50:17 -0600

    comparar v2.0

--- app/Http/Controllers/ComparativaController.php
+++ app/Http/Controllers/ComparativaController.php
@@ -4,33 +4,97 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Productos;
+use App\ProductosPedidos;
+use App\Pedidos;
+use App\Detalles;
 use App\Salidas;
+use Barryvdh\DomPDF\Facade as PDF;
+use Carbon\Carbon;
 
 class ComparativaController extends Controller
 {
     //
+    public function cargar()
+    {
+        $productos = DB::table('tbl_producto')
+            ->select('nombre')
+            ->get();
+        $fecha = '';
+        $pdf = 0;
+        $fechaini = '';
+        $fechafin = '';
+        return view('reportes.estrategicos.compararGanancia',compact('fecha','pdf','fechaini','fechafin',
+                        'productos'));
+    }
+
+    public function comparativaPreview()
+    {
+        $fecha = session('fecha');
+        $pdf = session('pdf');
+        $fechaini = session('fechaini');
+        $fechafin = session('fechafin');
+        $datos = session('datos');
+        $fechaventa = $datos[0];
+        $preciounitario = $datos[1];
+        $unidades = $datos[2];
+        $subtotal = $datos[3];
+        $descuento = $datos[4];
+        $total = $datos[5];
+
+        return view('reportes.estrategicos.compararGanancia')->with(compact('fecha','pdf','fechaini',
+        'fechafin','fechaventa','preciounitario','unidades','subtotal','descuento','total'));
+    }
+
     public function comp(Request $request)
     {
         $this->validate($request,[
-            'fechaini1' => 'required',
-            'fechaini2' => 'required',
-            'fechafin1' => 'required | after_or_equal:fechaini1',
-            'fechafin2' => 'required | after_or_equal:fechaini2',
+            'fechaini' => 'required',
+            'fechafin' => 'required | after_or_equal:fechaini',
+            'producto' => 'required'
         ]);
 
-        $fechaini1 = $request->input('fechaini1');
-        $fechaini2 = $request->input('fechaini2');
-        $fechafin1 = $request->input('fechafin1');
-        $fechafin2 = $request->input('fechafin2');
+        $fechaini = $request->input('fechaini');
+        $fechafin = $request->input('fechafin');
+        $producto = $request->input('producto');
 
-        $monto1 = DB::table('tbl_salida')
-            ->whereBetween('fecha_emision',[$fechaini1,$fechafin1])
-            ->sum('total');
-        $monto2 = DB::table('tbl_salida')
-            ->whereBetween('fecha_emision',[$fechaini2,$fechafin2])
-            ->sum('total');
+        if($fechaini > $fechafin){
+            $fechaini='';
+            $fechafin='';
+            return redirect()->route('compararGanancia.pantalla')->withErrors(['La fecha inicial es mayor 
+                        que la fecha final.']);
+        }
 
-        return view('reportes.estrategicos.mostrarComparativa',compact('monto1','monto2','fechaini1'
-                ,'fechaini2','fechafin1','fechafin2'));
+        $fecha = Carbon::parse(Carbon::now())->format('d/m/Y');
+        $success = 'Comparación generada';
+        $pdf = 1;
+        $datos = $this->generarComparativa($fechaini,$fechafin,$producto);
+        $fechaventa = $datos[0];
+        $preciounitario = $datos[1];
+        $unidades = $datos[2];
+        $subtotal = $datos[3];
+        $descuento = $datos[4];
+        $total = $datos[5];
+
+        return view('reportes.estrategicos.compararGanancia')->with(compact('fecha','pdf','fechaini',
+                    'fechafin','fechaventa','preciounitario','unidades','subtotal','descuento','total'));
+    }
+
+    public function generarComparativa($fechaini,$fechafin,$producto)
+    {
+        $datos = array();
+        $ventas = DB::table('tbl_salida')
+        ->join('tbl_detalle','tbl_salida.id','tbl_detalle.salida_id')
+        ->join('tbl_pedido','tbl_pedido.id','tbl_detalle.pedido_id')
+        ->join('tbl_producto_pedido','tbl_producto_pedido.producto_id','tbl_pedido.id')
+        ->join('tbl_producto','tbl_producto.id','tbl_producto_pedido.producto_id')
+        ->select('tbl_salida.fecha_emision','tbl_producto_pedido AS preciounitario',
+        'tbl_detalle.cantidad_vendida AS unidades','tbl_detalle.total_detalle AS subtotal',
+        'tbl_detalle.total_con_desc AS descuento','tbl_salida.total_iva')
+        ->where('tbl_producto.nombre',$producto)
+        ->where('tbl_salida.fecha_emision',[$fechaini,$fechafin])
+        ->get()->toArray();
+        //dd($ventas);
+        return $datos;
     }
 }
--- resources/views/reportes/estrategicos/compararGanancia.blade.php
+++ resources/views/reportes/estrategicos/compararGanancia.blade.php
@@ -13,54 +13,89 @@ Comparativa de Ventas
     <div class="span12">
         <div class="info-box">
             <div class="row-fluid stats-box">
-                @if ($errors->any())
-                <div class="alert alert-danger">
-					<strong>Error! </strong>Datos erroneos introducidos.<br>
-					La fecha final no puede ser anterior a la inicial.
-				</div>
-                @endif
                 <div class="span12">
                     <center><h3>Librería el Páramo</h3></center>
                     <center><h3>Unidad administrativa</h3></center>
-                    <center><h3>Comparativa de Ventas</h3></center>
+                    <center><h3>Informe de comparación de ventas de producto</h3></center>
                 </div>
                 <div class="span12">
-                    <form action="{{ route('compararGanancia') }}" method="POST">
+                    <h3>Fecha de generación: {{$fecha}}</h3>
+                </div>
+                @if ($message = Session::get('success'))
+				<div class="alert alert-success">
+					<p>{{ $message }}</p>
+				</div>
+				@endif
+                @if ($errors->any())
+				<div class="alert alert-danger">
+					<strong>Error! </strong>Datos invalidos introducidos.<br><br>
+					<ul>
+						@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+				@endif
+                <div class="span12">
+                    <form action="{{route('compararGanancia.comp')}}" method="GET" form="comparativa">
                         @csrf
-                        <div class="span12">
-                            <h3>Ingrese el primer periodo:</h3>
-                        </div>
-                        <div class="span4">
+                        <div class="span3">
                             <div class="form-group">
-                                <h3 style="display: inline-block; width: 120px">Fecha Inicio: </h3>
-                                <input type="date" value="fechaini1" class="form-control" name="fechaini1" required>
+                                <h3 style="display: inline-block; width: 120px">Desde: </h3>
+                                <input type="date" value="{{$fechaini}}" class="form-control" name="fechaini" required>
                             </div>
                         </div>
-                        <div class="span4">
+                        <div class="span3">
                             <div class="form-group">
-                                <h3 style="display: inline-block; width: 120px">Fecha Fin: </h3>
-                                <input type="date" value="fechafin1" class="form-control" name="fechafin1" required>
+                                <h3 style="display: inline-block; width: 120px">Hasta: </h3>
+                                <input type="date" value="{{$fechafin}}" class="form-control" name="fechafin" required>
                             </div>
                         </div>
-                        <div class="span12">
-                            <h3>Ingrese el segundo periodo:</h3>
-                        </div>
-                        <div class="span4">
+                        <div class="span3">
                             <div class="form-group">
-                                <h3 style="display: inline-block; width: 120px">Fecha Inicio: </h3>
-                                <input type="date" value="fechaini2" class="form-control" name="fechaini2" required>
+                                <h3 style="display: inline-block; width: 120px">Produto: </h3>
+                                <select name="producto" class="custom-select" required>
+                                    <option selected="">Selecciona</option>
+                                    @foreach ($productos as $key => $producto)
+                                        <option value="{{$producto->nombre}}">{{$producto->nombre}}</option>
+                                    @endforeach
+                                </select>
                             </div>
-                        </div>
-                        <div class="span4">
-                            <div class="form-group">
-                                <h3 style="display: inline-block; width: 120px">Fecha Fin: </h3>
-                                <input type="date" value="fechafin2" class="form-control" name="fechafin2" required>
-                            </div>
-                        </div>
-                        <div class="span12">
-                            <button type="submit" class="btn btn-info">Aceptar</button>
                         </div>
                     </form>
+                </div>
+                <div class="span3">
+                        <button type="submit" class="btn btn-info" form="comparativa">Generar Reporte</button>
+                </div>
+                <div class="span3">
+					@if($pdf == 0)
+					<button  disabled="" class="btn btn-danger">Imprimir PDF</button>
+					@else
+					<a href="{{route('ganancias.pdf',['fechaini'=>$fechaini,'fechafin'=>$fechafin])}}" class="btn btn-danger">Imprimir PDF</a>
+					@endif
+				</div>
+                <div class="span3">
+                    <a href="{{url('/')}}" title="" class="btn btn-secondary">Regresar</a>
+                </div>
+                <div class="widget widget-table action-table">
+                    <div class="widget-header"><i class="icon-th-list"></i></div>
+                    <div class="widget-content">
+                        <table class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th width="20%">Fecha venta</th>
+                                    <th width="15%">Cantidad unidades</th>
+                                    <th width="15%">P/U</th>
+                                    <th width="10%">Subtotal</th>
+                                    <th width="20%">Descuento</th>
+                                    <th width="20%">Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
